cfddd74a8668fad0e7a9fe5c9bd4123b09504c13
Bhagwat Singh Chouhan <bhagwat.chouhan@gmail.com>
2020-07-11 22:06:10 +0530

    Added the database files.

---
+++ newsletter.sql
@@ -0,0 +1,303 @@
+CREATE DATABASE  IF NOT EXISTS `newsletter` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `newsletter`;
+-- MySQL dump 10.13  Distrib 8.0.14, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: newsletter
+-- ------------------------------------------------------
+-- Server version	8.0.14
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `address`
+--
+
+DROP TABLE IF EXISTS `address`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `address` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `userId` bigint(20) DEFAULT NULL,
+  `subscriberId` bigint(20) DEFAULT NULL,
+  `firstName` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `middleName` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `lastName` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `mobile` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `line1` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `line2` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `city` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `province` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `country` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `areaCode` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_address_user` (`userId`),
+  KEY `idx_address_subscriber` (`subscriberId`),
+  CONSTRAINT `fk_address_subscriber` FOREIGN KEY (`subscriberId`) REFERENCES `subscriber` (`id`),
+  CONSTRAINT `fk_address_user` FOREIGN KEY (`userId`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `address`
+--
+
+LOCK TABLES `address` WRITE;
+/*!40000 ALTER TABLE `address` DISABLE KEYS */;
+/*!40000 ALTER TABLE `address` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `edition`
+--
+
+DROP TABLE IF EXISTS `edition`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `edition` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `newsletterId` bigint(20) NOT NULL,
+  `title` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(2048) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` smallint(6) NOT NULL DEFAULT '0',
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  `publishedAt` datetime DEFAULT NULL,
+  `content` text COLLATE utf8mb4_unicode_ci,
+  PRIMARY KEY (`id`),
+  KEY `idx_edition_newsletter` (`newsletterId`),
+  CONSTRAINT `fk_edition_newsletter` FOREIGN KEY (`newsletterId`) REFERENCES `newsletter` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `edition`
+--
+
+LOCK TABLES `edition` WRITE;
+/*!40000 ALTER TABLE `edition` DISABLE KEYS */;
+/*!40000 ALTER TABLE `edition` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mailing_list`
+--
+
+DROP TABLE IF EXISTS `mailing_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `mailing_list` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `newsletterId` bigint(20) NOT NULL,
+  `subscriberId` bigint(20) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_mlist_newsletter` (`newsletterId`),
+  KEY `idx_mlist_subscriber` (`subscriberId`),
+  CONSTRAINT `fk_mlist_newsletter` FOREIGN KEY (`newsletterId`) REFERENCES `newsletter` (`id`),
+  CONSTRAINT `fk_mlist_subscriber` FOREIGN KEY (`subscriberId`) REFERENCES `subscriber` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mailing_list`
+--
+
+LOCK TABLES `mailing_list` WRITE;
+/*!40000 ALTER TABLE `mailing_list` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mailing_list` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `newsletter`
+--
+
+DROP TABLE IF EXISTS `newsletter`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `newsletter` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `userId` bigint(20) NOT NULL,
+  `title` varchar(75) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descritpion` varchar(2048) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `type` smallint(6) NOT NULL DEFAULT '0',
+  `multiple` tinyint(1) NOT NULL DEFAULT '0',
+  `global` tinyint(1) NOT NULL DEFAULT '0',
+  `status` smallint(6) NOT NULL DEFAULT '0',
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  `publishedAt` datetime DEFAULT NULL,
+  `content` text COLLATE utf8mb4_unicode_ci,
+  PRIMARY KEY (`id`),
+  KEY `idx_newsletter_user` (`userId`),
+  CONSTRAINT `fk_newsletter_user` FOREIGN KEY (`userId`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `newsletter`
+--
+
+LOCK TABLES `newsletter` WRITE;
+/*!40000 ALTER TABLE `newsletter` DISABLE KEYS */;
+/*!40000 ALTER TABLE `newsletter` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `newsletter_meta`
+--
+
+DROP TABLE IF EXISTS `newsletter_meta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `newsletter_meta` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `newsletterId` bigint(20) NOT NULL,
+  `type` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `key` varchar(160) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `content` text COLLATE utf8mb4_unicode_ci,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_pnewsletter_meta` (`newsletterId`,`key`),
+  KEY `idx_meta_newsletter` (`newsletterId`),
+  CONSTRAINT `fk_meta_newsletter` FOREIGN KEY (`newsletterId`) REFERENCES `newsletter` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `newsletter_meta`
+--
+
+LOCK TABLES `newsletter_meta` WRITE;
+/*!40000 ALTER TABLE `newsletter_meta` DISABLE KEYS */;
+/*!40000 ALTER TABLE `newsletter_meta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `newsletter_trigger`
+--
+
+DROP TABLE IF EXISTS `newsletter_trigger`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `newsletter_trigger` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `newsletterId` bigint(20) NOT NULL,
+  `subscriberId` bigint(20) NOT NULL,
+  `sent` tinyint(1) NOT NULL DEFAULT '1',
+  `delivered` tinyint(1) NOT NULL DEFAULT '1',
+  `mode` smallint(6) NOT NULL DEFAULT '0',
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  `sentAt` datetime DEFAULT NULL,
+  `deliveredAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_trigger_newsletter` (`newsletterId`),
+  KEY `idx_trigger_subscriber` (`subscriberId`),
+  CONSTRAINT `fk_trigger_newsletter` FOREIGN KEY (`newsletterId`) REFERENCES `newsletter` (`id`),
+  CONSTRAINT `fk_trigger_subscriber` FOREIGN KEY (`subscriberId`) REFERENCES `subscriber` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `newsletter_trigger`
+--
+
+LOCK TABLES `newsletter_trigger` WRITE;
+/*!40000 ALTER TABLE `newsletter_trigger` DISABLE KEYS */;
+/*!40000 ALTER TABLE `newsletter_trigger` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `subscriber`
+--
+
+DROP TABLE IF EXISTS `subscriber`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `subscriber` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `customerId` bigint(20) DEFAULT NULL,
+  `firstName` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `middleName` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `lastName` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mobile` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_sub_cust_email` (`customerId`,`email`),
+  KEY `idx_subscriber_customer` (`customerId`),
+  CONSTRAINT `fk_subscriber_customer` FOREIGN KEY (`customerId`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `subscriber`
+--
+
+LOCK TABLES `subscriber` WRITE;
+/*!40000 ALTER TABLE `subscriber` DISABLE KEYS */;
+/*!40000 ALTER TABLE `subscriber` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `middleName` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `lastName` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `mobile` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `passwordHash` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `admin` tinyint(1) NOT NULL DEFAULT '0',
+  `customer` tinyint(1) NOT NULL DEFAULT '0',
+  `registeredAt` datetime NOT NULL,
+  `lastLogin` datetime DEFAULT NULL,
+  `intro` tinytext COLLATE utf8mb4_unicode_ci,
+  `profile` text COLLATE utf8mb4_unicode_ci,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_mobile` (`mobile`),
+  UNIQUE KEY `uq_email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-07-11 21:36:03
